3133954d741ea6dec0548bc346ab2d11a37eb64d
Muhammad Ali <81400350+rahmanreza646@users.noreply.github.com>
2021-10-25 19:06:48 +0600

    Add files via upload

---
+++ login.php
@@ -0,0 +1,153 @@
+<?php
+
+  session_start();
+  if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header("location: homepage.php");
+    exit;
+  }
+
+  require_once "config.php";
+
+  $username = $password = "";
+  $username_err = $password_err = "";
+
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    
+    if(empty(trim($_POST['username']))){
+      $username_err = 'Please enter username.';
+    } else{
+      $username = trim($_POST['username']);
+    }
+
+    if(empty(trim($_POST['password']))){
+      $password_err = 'Please enter your password.';
+    } else{
+      $password = trim($_POST['password']);
+    }
+
+    if (empty($username_err) && empty($password_err)) {
+    
+      $sql = 'SELECT id, username, password FROM users WHERE username = ?';
+
+      if ($stmt = $conn->prepare($sql)) {
+
+        $param_username = $username;
+        $stmt->bind_param('s', $param_username);
+
+        if ($stmt->execute()) {
+          $stmt->store_result();
+
+          if ($stmt->num_rows == 1) {
+            
+            $stmt->bind_result($id, $username, $hashed_password);
+            if ($stmt->fetch()) {
+              if (password_verify($password, $hashed_password)) {
+
+                session_start();
+
+                $_SESSION['loggedin'] = true;
+                $_SESSION['id'] = $id;
+                $_SESSION['username'] = $username;
+                
+                header('location: homepage.php');
+              } else {
+
+                $password_err = 'Invalid password';
+
+              }
+            }
+          } else {
+
+            $username_err = "Username does not exists.";
+
+          }
+        } else {
+          echo "Oops! Something went wrong please try again";
+        }
+       
+        $stmt->close();
+      }
+
+      $conn->close();
+    }
+  }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="author" content="Kodinger">
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+	<title>My Login Page</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/my-login.css">
+</head>
+
+<body class="my-login-page">
+
+	<section class="h-100">
+		<div class="container h-100">
+			<div class="row justify-content-md-center h-100">
+				<div class="card-wrapper">
+					<div class="brand">
+						
+					</div>
+					<div class="card fat">
+						<div class="card-body">
+							<h4 class="card-title">Login</h4>
+
+							<form action="homepage.php"   method="post" class="my-login-validation" >
+								<div class="form-group">
+									<label for="email">Email</label>
+									<input id="email" type="email" class="form-control" name="email" value="" required autofocus >
+									<div class="invalid-feedback">
+										invalid email
+									</div>
+								</div>
+
+								<div class="form-group">
+									<label for="password">Password
+										<a href="forgot.html" class="float-right">
+											forgot password?
+										</a>
+									</label>
+									<input id="password" type="password" class="form-control" name="password" required data-eye>
+								    <div class="invalid-feedback">
+								    	password is required
+							    	</div>
+								</div>
+
+								<div class="form-group">
+									<div class="custom-checkbox custom-control">
+										<input type="checkbox" name="remember" id="remember" class="custom-control-input">
+										<label for="remember" class="custom-control-label">Remember Me</label>
+									</div>
+								</div>
+
+								<div class="form-group m-0">
+									<button type="submit" class="btn btn-primary btn-block"  >
+										Login
+									</button>
+								</div>
+								<div class="mt-4 text-center">
+									Don't have an account? <a href="register.php">Register</a>
+								</div>
+							</form>
+						</div>
+					</div>
+					<div class="footer">
+						Copyright &copy; 2021 __ K Poxoul 
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<script src="js/my-login.js"></script>
+</body>
+</html>
