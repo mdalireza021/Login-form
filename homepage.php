135f393443f216af3fb381fa840cbaf72b5b9b22
Muhammad Ali <81400350+rahmanreza646@users.noreply.github.com>
2021-10-25 18:57:19 +0600

    Add files via upload

---
+++ homepage.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" />
+	<link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css" rel="stylesheet" />
+	
+	<title>Dashboard</title>
+
+</head>
+<body>
+
+<header class="navbar navbar-inverse navbar-fixed-top bs-docs-nav" role="banner">
+  <div class="container">
+    <div class="navbar-header">
+      <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a href="./" class="navbar-brand">Bootstrap Menu</a>
+    </div>
+    <nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
+      <ul class="nav navbar-nav navbar-right">
+        <li>
+          <a href="#">Home</a>
+        </li>
+       
+        <li>
+          <a href="#">Products</a>
+        </li>
+        <li>
+          <a href="#">Services</a>
+        </li>
+        <li >
+          <a href="#">Contact</a>
+        </li>
+		<li >
+          <a href="#">About</a>
+        </li>
+		<li >
+          <a href="logout.php">Logout</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</header>
+</body>
+</html>
